998966bead93001d50723e476b72800574d2ff50
Daria Demskaya <dariademskaya@MacBook-Pro.local>
2023-10-24 22:39:27 +0300

    Task 3.1

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -1,8 +1,11 @@
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.net.URL;
@@ -14,11 +17,11 @@ public class FirstTest {
     public void setUp() throws Exception {
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("platformName", "Android");
-        capabilities.setCapability("deviceName", "Pixel6");
+        capabilities.setCapability("deviceName", "Pixel5");
         capabilities.setCapability("platformVersion", "8.0");
         capabilities.setCapability("automationName", "Appium");
         capabilities.setCapability("appPackage", "org.wikipedia");
-        capabilities.setCapability("appActvity", ".main.MainActivity");
+        capabilities.setCapability("appActvity", ".`main.MainActivity`");
         capabilities.setCapability("app", "/Users/dariademskaya/Documents/JavaAppiumAutomation/JavaAppiumAutomation/apks/org.wikipedia.apk");
 
         URL appiumServerURL = new URL("http://127.0.0.1:4723/wd/hub");
@@ -30,8 +33,20 @@ public class FirstTest {
     public void tearDown() {
         driver.quit();
     }
+
     @Test
     public void myTest() {
-        System.out.println("This is my first test");
+        assertElementHasText(
+                By.xpath("//*[@resource-id='org.wikipedia:id/search_container']//*[@class='android.widget.TextView']"),
+                "Search Wikipedia",
+                "Search input contains incorrect text");
     }
+
+    public void assertElementHasText(By by, String expected_text, String error_message) {
+      WebElement element = driver.findElement(by);
+      String actual_text = element.getText();
+      Assert.assertEquals(error_message, expected_text, actual_text);
+    }
+
+
 }
