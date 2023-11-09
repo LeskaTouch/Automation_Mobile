146979f35f5e6474c7ba975214b33e3ac486764a
LeskaTouch <lesyatouch@gmail.com>
2023-11-09 11:09:05 +0300

    Task 5.2

--- src/lib/ui/SearchPageObject.java
+++ src/lib/ui/SearchPageObject.java
@@ -4,6 +4,7 @@ import io.appium.java_client.MobileDriver;
 import org.omg.CORBA.INTERNAL;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class SearchPageObject extends MainPageObject {
 
@@ -13,6 +14,9 @@ public class SearchPageObject extends MainPageObject {
     private static final String CLEAR_BUTTON_XPATH = "org.wikipedia:id/search_close_btn";
     private static final String SEARCH_RESULT_ITEM_TITLE_XPATH = "(//*[@resource-id='org.wikipedia:id/search_results_list']//*[@resource-id='org.wikipedia:id/page_list_item_title'])[{ORDER}]";
     private static final String ARTICLE_NAME_XPATH_TEMPLATE = "//*[@resource-id='org.wikipedia:id/page_list_item_title'][@text = '{TITLE}']";
+
+    private static final String ARTICLE_TITLE_AND_DESCRIPTION_XPATH = "//*[@resource-id='org.wikipedia:id/search_results_list']//*[@class='android.view.ViewGroup'][*[@text = '{DESCRIPTION}'] and *[@text='{TITLE}']]";
+
 
     public SearchPageObject(MobileDriver driver){
         super(driver);
@@ -65,17 +69,38 @@ public class SearchPageObject extends MainPageObject {
                 expected_text + " is not present in each line");
     }
 
-    private String getSearchResultItemTitleXpath(int order){
-        return SEARCH_RESULT_ITEM_TITLE_XPATH.replace("{ORDER}", Integer.toString(order));
-    }
+
 
     public void clickArticleName(String article_id){
-        String article_xpath = ARTICLE_NAME_XPATH_TEMPLATE.replace("{TITLE}", article_id);
+        String article_xpath = getArticleNameXpath(article_id);
+
         waitForElementAndClick(
                 By.xpath(article_xpath),
                 "No article is found for: " + article_id,
                 5);
 
     }
+
+
+    public WebElement waitForElementByTitleAndDescription(String title, String description) {
+        return waitForElement(
+                By.xpath(getSearchResultItemByTitleAndDescription(title, description)),
+                "No article with TITLE " + title + " and DESCRIPTION " + description + " is found",
+                5);
+    }
+
+    // TEMPLATES METHODS
+    private String getSearchResultItemTitleXpath(int order){
+        return SEARCH_RESULT_ITEM_TITLE_XPATH.replace("{ORDER}", Integer.toString(order));
+    }
+    private String getArticleNameXpath(String title){
+        return ARTICLE_NAME_XPATH_TEMPLATE.replace("{TITLE}", title);
+
+    }
+
+    private String getSearchResultItemByTitleAndDescription(String title, String description){
+        return  ARTICLE_TITLE_AND_DESCRIPTION_XPATH.replace("{TITLE}", title).replace ("{DESCRIPTION}", description);
+    }
+    // TEMPLATES METHODS
 
 }
--- src/tests/SearchTests.java
+++ src/tests/SearchTests.java
@@ -2,6 +2,7 @@ package tests;
 
 import lib.CoreTestCase;
 import lib.ui.SearchPageObject;
+import org.openqa.selenium.WebElement;
 
 public class SearchTests extends CoreTestCase {
     public void testClearResult(){
@@ -29,6 +30,17 @@ public class SearchTests extends CoreTestCase {
         searchPageObject.assertSearchResultHasText("Java", 4);
         searchPageObject.assertSearchResultHasText("Java", 5);
         searchPageObject.assertSearchResultHasText("Java", 6);
+
+    }
+
+    public void testSearchArticleWithTitleAndDescription(){
+        SearchPageObject searchPageObject = new SearchPageObject(driver);
+        searchPageObject.initSearchElement();
+        searchPageObject.typeSearchLine("Java");
+
+        searchPageObject.waitForElementByTitleAndDescription("Java", "Island in Indonesia");
+        searchPageObject.waitForElementByTitleAndDescription("JavaScript", "High-level programming language");
+        searchPageObject.waitForElementByTitleAndDescription("Java (programming language)", "Object-oriented programming language");
 
     }
 }
